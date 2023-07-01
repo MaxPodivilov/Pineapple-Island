9ba85da15297b88be0ecd6866587f8546793c17b
MaxPodivilov <119669516+MaxPodivilov@users.noreply.github.com>
2023-07-01 12:44:50 +0700

    Доработал меню

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let menuBtn = document.querySelector(".menu-btn");
 let menu = document.querySelector(".nav");
 let menuItem = document.querySelectorAll(".nav__link");
+let body = document.querySelector("body");
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
+  body.classList.toggle("active")
 });
 
 menuItem.forEach(function (menuItem) {
   menuItem.addEventListener("click", function () {
     menuBtn.classList.toggle("active");
     menu.classList.toggle("active");
+    body.classList.toggle("active")
   });
 });
