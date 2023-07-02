b01519e8af1268c76d8fb51627a1595e2b6b1f97
MaxPodivilov <119669516+MaxPodivilov@users.noreply.github.com>
2023-07-02 23:32:11 +0700

    Доработана форма отправки

--- js/popap.js
+++ js/popap.js
@@ -1,11 +1,28 @@
-let openPopupButton = document.getElementById('openPopupButton');
-let popup = document.getElementById('popup');
+function validateForm(event) {
+  event.preventDefault(); // Отменяем стандартное поведение отправки формы
 
-openPopupButton.addEventListener('click', () => {
-  console.log('click');
+  const nameInput = document.getElementById('Name');
+  const phoneInput = document.getElementById('Phone');
+  const emailInput = document.getElementById('Email');
+
+  if (nameInput.value === '' || phoneInput.value === '' || emailInput.value === '') {
+    // Если хотя бы одно из полей не заполнено, выдаем сообщение об ошибке
+    alert('Пожалуйста, заполните все поля формы.');
+    return false; // Отменяем отправку формы
+  }
+
+  showPopup(); // Показываем попап только если все поля заполнены
+
+  return false; // Отменяем отправку формы
+}
+
+function showPopup() {
+  const popup = document.getElementById('popup');
   popup.style.display = 'block';
-  
+
   setTimeout(() => {
     popup.style.display = 'none';
   }, 3000); // Попап будет скрываться через 3 секунды (3000 миллисекунд)
-});
+}
+
+document.querySelector('.feedback').addEventListener('submit', validateForm);
